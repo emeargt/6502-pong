36ab5b6fd10cd8a67eb00af7152fc3c6d0404416
emeargt <graemet@uvic.ca>
2020-08-22 12:44:13 -0700

    Add comments to NES header bytes

--- pong.asm
+++ pong.asm
@@ -1,14 +1,14 @@
 .segment "HEADER"
   .byte "NES"
   .byte $1a
-  .byte $02
-  .byte $01
-  .byte %00000000
+  .byte $02			; 2 * 16KB PRG ROM
+  .byte $01			; 1 * 8KB CHR ROM
+  .byte %00000000		; mapper and mirroring
   .byte $00
   .byte $00
   .byte $00
   .byte $00
-  .byte $00,$00,$00,$00,$00
+  .byte $00,$00,$00,$00,$00	; filler bytes
 
 ;;;;;;;;;;;;;
 
@@ -81,12 +81,12 @@ VBLANK:
   LDA #$00
   STA counter
 SkipColourChange:
-  RTI
+  RTI		; return from interrupt
 
 ;;;;;;;;;;;;;;;;
 
 .segment "VECTORS"
-  .word VBLANK	; vblank vector
+  .word VBLANK	; vblank vector, screen redrawn
   .word RESET	; use label RESET when processor first turns on
 		; or is reset
   .word 0	; external interrupt IRQ undefined right now
